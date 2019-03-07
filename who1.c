acccc8ab4584ee1d852b56daa67982869a6ccffe
Li-Fish <1136727300@qq.com>
2019-03-08 04:56:52 +0800

    实现who的时间格式

---
+++ who1.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <utmp.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+#define SHOWHOST
+
+void showtime(long);
+void show_info(struct utmp *);
+
+int main() {
+    struct utmp current_record;
+    int utmpfd;
+    int reclen = sizeof(current_record);
+    if ((utmpfd = open(UTMP_FILE,  O_RDONLY) ) == -1) {
+        perror(UTMP_FILE);
+        exit(1);
+    }
+    while (read(utmpfd, &current_record, reclen) == reclen) {
+        show_info(&current_record);
+    }
+    close(utmpfd);
+}
+
+void show_info(struct utmp *utbufp) {
+    if (utbufp->ut_type != USER_PROCESS) return;
+    printf("%-8.8s ", utbufp->ut_name);
+    printf("%-8.8s ", utbufp->ut_line);
+    showtime(utbufp->ut_time);
+    // printf("%-10ld ", utbufp->ut_time);
+#ifdef SHOWHOST
+    printf("(%s)", utbufp->ut_host);
+#endif
+    printf("\n");
+}
+
+void showtime(long timeval) {
+    char *cp;
+    cp = ctime(&timeval);
+    printf("%12.12s", cp + 4);
+}
