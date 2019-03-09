fe138cb9b2e90183a141693c7f48b5f7fd889200
Li-Fish <1136727300@qq.com>
2019-03-09 23:21:22 +0800

    who增加缓冲区

--- who1.c
+++ who1.c
@@ -4,23 +4,64 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <time.h>
+
+#define NRECS 16
+#define NULLUT ((struct utmp *) NULL)
+#define UTSIZE (sizeof(struct utmp))
+
+static char utmpbuf[NRECS * UTSIZE];
+static int num_recs;
+static int cur_rec;
+static int fd_utmp = -1;
+
+int utmp_open(char *file_name) {
+    fd_utmp = open(file_name, O_RDONLY);
+    cur_rec = num_recs = 0;
+    return fd_utmp;
+}
+
+int utmp_reload() {
+    int amt_read;
+    amt_read = read(fd_utmp, utmpbuf, NRECS * UTSIZE);
+    num_recs = amt_read / UTSIZE;
+    cur_rec = 0;
+    return num_recs;
+}
+
+struct utmp *utmp_next() {
+    struct utmp *recp;
+    if (fd_utmp == -1) {
+        return NULLUT;
+    }
+    if (cur_rec == num_recs && utmp_reload() == 0) {
+        return NULLUT;
+    }
+    recp = (struct utmp*) &utmpbuf[cur_rec * UTSIZE];
+    cur_rec++;
+    return recp;
+}
+
+void utmp_close() {
+    if (fd_utmp != -1) {
+        close(fd_utmp);
+    }
+}
+
 #define SHOWHOST
 
 void showtime(long);
 void show_info(struct utmp *);
 
 int main() {
-    struct utmp current_record;
-    int utmpfd;
-    int reclen = sizeof(current_record);
-    if ((utmpfd = open(UTMP_FILE,  O_RDONLY) ) == -1) {
+    struct utmp *utbufp;
+    if (utmp_open(UTMP_FILE) == -1) {
         perror(UTMP_FILE);
         exit(1);
     }
-    while (read(utmpfd, &current_record, reclen) == reclen) {
-        show_info(&current_record);
+    while ((utbufp = utmp_next()) != NULLUT) {
+        show_info(utbufp);
     }
-    close(utmpfd);
+    utmp_close();
 }
 
 void show_info(struct utmp *utbufp) {
@@ -28,7 +69,6 @@ void show_info(struct utmp *utbufp) {
     printf("%-8.8s ", utbufp->ut_name);
     printf("%-8.8s ", utbufp->ut_line);
     showtime(utbufp->ut_time);
-    // printf("%-10ld ", utbufp->ut_time);
 #ifdef SHOWHOST
     printf("(%s)", utbufp->ut_host);
 #endif
